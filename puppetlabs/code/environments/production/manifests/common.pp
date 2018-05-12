480674c9a5eba4662d5c0e8d11ab93a0499e89d1
MamaTried <33388729+mamatr13d@users.noreply.github.com>
2018-05-11 23:53:07 -0700

    Revert "init commit"
    This reverts commit 09bec02819034c8b97596f0367f2eaa1f34dee7a.

--- puppetlabs/code/environments/production/manifests/common.pp
+++
@@ -1,7 +0,0 @@
-class common {
-  include accounts
-  include chrony
-#  include dotfiles
-  include epel
-  include linux_packages
-}
--- puppetlabs/code/environments/production/manifests/vcsrepo.pp
+++
@@ -1,7 +0,0 @@
-vcsrepo { '/home/pbauer/.oh-my-zsh':
-  ensure   => present,
-  provider => git,
-  user     => 'pbauer',
-  group    => 'pbauer',
-  source   => 'https://github.com/robbyrussell/oh-my-zsh.git',
-}
